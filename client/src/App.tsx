29bed4e4442f969a8fc628cc5fa4542924f9b4cf
Jonathan Alcantara Martinez <jonathan.informatics@gmail.com>
2025-07-21 17:35:26 -0600

    Simplify routing to handle GitHub Pages paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,26 +6,17 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
 
-// Get base path for GitHub Pages
-const basePath = import.meta.env.PROD ? '/ReduxCounter' : '';
-
-function AppRouter() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <WouterRouter base={basePath}>
-          <AppRouter />
-        </WouterRouter>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/ReduxCounter" component={Home} />
+          <Route path="/ReduxCounter/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
       </TooltipProvider>
     </QueryClientProvider>
   );
